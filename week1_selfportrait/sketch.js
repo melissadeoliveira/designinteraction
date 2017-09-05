c0fd2085353cbf68e8d93e628c24a7e376b9389d
melissadeoliveira <melissa_deoliveira@fitnyc.edu>
2017-09-05 19:23:17 -0400

    Initial upload, week 1 homework

---
+++ week1_selfportrait/sketch.js
@@ -0,0 +1,33 @@
+function setup(){
+    createCanvas(1280,1024);   
+}
+function draw(){
+    background(180,242,245);
+    fill(115,70,73);
+    noStroke();
+    rect(440,469,400,183);
+    ellipse(640,475,400,400);
+    fill(255);
+    ellipse(640,490,340,340);
+    fill(115,70,73);
+    noStroke();
+    rect(526,390,64,19);
+    rect(691,390,64,19);
+    fill(255,84,80);
+    noStroke();
+    rect(608,460,64,19);
+    fill(255,181,180)
+    ellipse(625,560,44.667,44.667)
+    ellipse(655,560,44.667,44.667)
+    triangle(607,573,640,604,673,573)
+    fill(255,181,180);
+    stroke(255,84,80);
+    strokeWeight(17);
+    ellipse(725,470,114,114)
+    fill(255,181,180);
+    stroke(255,84,80);
+    strokeWeight(17);
+    ellipse(555,470,114,114)
+    
+}
+    
